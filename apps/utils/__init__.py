328cec4e3e12fdb2bf7126c1fa4ca1e3773dac9e
zsdostar <zsdzzydos@gmail.com>
2017-05-21 20:17:47 +0800

    6-7 user register-3 part1 邮件发送的函数： 包括生成校验码， 发送邮件的主题与点击链接

---
+++ apps/utils/__init__.py
@@ -0,0 +1,4 @@
+# coding:utf8
+# __author__ = 'zsdostar'
+# __date__ = '2017/5/21 19:50'
+
---
+++ apps/utils/email_send.py
@@ -0,0 +1,34 @@
+# coding:utf8
+# __author__ = 'zsdostar'
+# __date__ = '2017/5/21 19:52'
+import string
+from random import Random
+from users.models import EmailVerifyRecord
+
+
+def random_str(randomlength=8):  # 生成随机校验码
+    str = ''
+    chars = string.ascii_letters + '0123456789'
+    length = len(chars) - 1
+    random = Random()
+    for i in range(randomlength):
+        str += chars[random.randint(0, length)]
+    return str
+
+
+def send_register_email(email, send_type='register'):  # 发送验证邮件
+    email_record = EmailVerifyRecord()  # 实例化email表
+    code = random_str(16)  # 16位校验码
+    email_record.code = code  # 传入数据库
+    email_record.email = email
+    email_record.send_type = send_type  # 类型为注册
+    email_record.save()
+
+    email_title = ''
+    email_body = ''
+
+    if send_type =='register':
+        email_title = '慕学在线网注册激活链接'
+        email_body = '请点击下边的链接激活你的账号: http://127.0.0.1:8000/active/{0}'.format(code)
+
+
