acfcf98e137047c5f7522a9cb1e9a4b366e79a41
zsdostar <zsdzzydos@gmail.com>
2017-05-26 10:33:05 +0800

    HTML中取路径的 连接路径

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -16,6 +16,6 @@ class OrgView(View):
         # 城市
         all_citys = CityDict.objects.all()
         return render(request, 'org-list.html', {
-            'all_orgs':all_orgs,
-            'all_citys':all_citys
+            'all_orgs': all_orgs,
+            'all_citys': all_citys
         })
