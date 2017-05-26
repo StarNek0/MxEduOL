718335e2a80d78d205837a0830ec7f147c0f8cc4
zsdostar <zsdzzydos@gmail.com>
2017-05-26 09:50:36 +0800

    把数据导入模板

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -15,4 +15,7 @@ class OrgView(View):
         all_orgs = CourseOrg.objects.all()
         # 城市
         all_citys = CityDict.objects.all()
-        return render(request, 'org-list.html', {})
+        return render(request, 'org-list.html', {
+            'all_orgs':all_orgs,
+            'all_citys':all_citys
+        })
