93b6652fcad85be7923d24bea89b126cc0ee9d07
zsdostar <zsdzzydos@gmail.com>
2017-05-26 09:49:32 +0800

    从models取出所有的相关数据

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -2,10 +2,17 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+from .models import CourseOrg
+from .models import CityDict
+
 
 class OrgView(View):
     """
     课程机构列表功能
     """
     def get(self, request):
+        # 课程机构
+        all_orgs = CourseOrg.objects.all()
+        # 城市
+        all_citys = CityDict.objects.all()
         return render(request, 'org-list.html', {})
