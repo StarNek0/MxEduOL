0054605b1d36bdb2482aafc4b6075ab8cf5d92c5
zsdostar <zsdzzydos@gmail.com>
2017-05-27 23:54:22 +0800

    配置ask_views的urls

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -3,10 +3,10 @@
 # __date__ = '2017/5/27 22:42'
 
 from django.conf.urls import url, include
-
+from .views import AddUserAskView
 
 from .views import OrgView
 urlpatterns = [
     url(r'^list/$', OrgView.as_view(), name='org_list'),
-    url(r'^add_ask/$')
+    url(r'^add_ask/$', AddUserAskView.as_view(), name='add_ask')
 ]
