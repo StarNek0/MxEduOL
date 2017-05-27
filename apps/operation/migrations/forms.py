168d75de76b719140c6cd4435549f516b9f2fcbf
zsdostar <zsdzzydos@gmail.com>
2017-05-27 22:40:06 +0800

    为用户咨询创建forms，另一种简单的创建forms的办法

---
+++ apps/operation/migrations/forms.py
@@ -0,0 +1,18 @@
+# coding:utf8
+# __author__ = 'zsdostar'
+# __date__ = '2017/5/27 22:30'
+from django import forms
+from operation.models import UserAsk
+
+
+class UserAskForm(forms.Form):
+    name = forms.CharField(required=True, min_length=2, max_length=30)
+    phone = forms.CharField(required=True, min_length=11, max_length=11)
+    course_name = forms.CharField(required=True, max_length=50)
+
+
+class AnotherUserAskForm(forms.ModelForm):
+
+    class Meta:
+        model = UserAsk
+        fields = ['name', 'mobile', 'course_name']
