d725f5cb56aa4523c3f884b860be86a4f62d1dc1
zsdostar <zsdzzydos@gmail.com>
2017-05-29 17:16:56 +0800

    media路径不显示图片。。只能去掉../的..

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -4,10 +4,10 @@
 
 from django.conf.urls import url, include
 
-from .views import OrgView, AddUserAskView
+from .views import OrgView, AddUserAskView, OrgHomeView
 
 urlpatterns = [
     url(r'^list/$', OrgView.as_view(), name='org_list'),
     url(r'^add_ask/$', AddUserAskView.as_view(), name='add_ask'),
-    url(r'^home/(?P<org_id>\d+)$', AddUserAskView.as_view(), name='add_ask')
+    url(r'^home/(?P<org_id>\d+)$', OrgHomeView.as_view(), name='add_ask')
 ]
