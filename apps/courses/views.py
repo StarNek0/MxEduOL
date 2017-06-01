ea0af9ba67db9698573c924ca5b6b16b19a3d124
zsdostar <zsdzzydos@gmail.com>
2017-06-01 17:39:40 +0800

    从models取出所有数据并传递到HTML页面

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -2,7 +2,11 @@
 from django.shortcuts import render
 from django.views.generic.base import View
 
+from .models import Course
 
 class CourseListView(View):
     def get(self, request):
-        return render(request, 'course-list.html', {})
+        all_courses = Course.objects.all()
+        return render(request, 'course-list.html', {
+            'all_courses': all_courses
+        })
