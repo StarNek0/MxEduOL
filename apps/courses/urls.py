478f04613e37e4e0b2b5255f99206cd5fabec5f5
zsdostar <zsdzzydos@gmail.com>
2017-06-04 23:55:18 +0800

    修改网页跳转路径，debugZZ错误

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -9,5 +9,5 @@ from .views import CourseListView, CourseDetailView
 urlpatterns = [
     url(r'^list/$', CourseListView.as_view(), name='course_list'),
     # 课程详情
-    url(r'^detail/(?P<course_id>\d+)$', CourseDetailView.as_view(), name='course_detail'),
+    url(r'^detail/(?P<course_id>\d+)/$', CourseDetailView.as_view(), name="course_detail"),
 ]
